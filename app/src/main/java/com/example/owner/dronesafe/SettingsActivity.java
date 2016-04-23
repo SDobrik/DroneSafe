05a336922f6f67068faf3f108a9ee52b993024d2
mrdofu <intense_smurf@live.com>
2016-04-23 12:10:56 -0400

    something to do with settings

--- app/src/main/java/com/example/owner/dronesafe/SettingsActivity.java
+++ app/src/main/java/com/example/owner/dronesafe/SettingsActivity.java
@@ -130,6 +130,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
      * Set up the {@link android.app.ActionBar}, if the API is available.
      */
     private void setupActionBar() {
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             // Show the Up button in the action bar.
