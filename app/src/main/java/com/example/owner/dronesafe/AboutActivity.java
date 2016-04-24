906acca82ec466c8159864364afd3fad9b918eb3
Vlad Cojanu <cojanu.vlad@gmail.com>
2016-04-24 10:19:44 -0400

    App icon added, about page kinda done

--- app/src/main/java/com/example/owner/dronesafe/AboutActivity.java
+++ app/src/main/java/com/example/owner/dronesafe/AboutActivity.java
@@ -5,18 +5,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_about);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            TextView myTextView = (TextView) findViewById(R.id.textView6);
 
-
-    }
+        }
 
 }
